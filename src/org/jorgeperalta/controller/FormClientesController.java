41c09f4d7179899778c45def2a519552764ea9ae
JorgePeralta6 <jperalta-2023478@kinal.edu.gt>
2024-04-25 15:44:45 -0600

    Se agrego el proyecto

---
+++ src/org/jorgeperalta/controller/FormClientesController.java
@@ -0,0 +1,143 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.jorgeperalta.controller;
+
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import org.jorgeperalta.dao.Conexion;
+import org.jorgeperalta.dto.ClienteDTO;
+import org.jorgeperalta.model.Cliente;
+import org.jorgeperalta.system.Main;
+
+/**
+ * FXML Controller class
+ *
+ * @author Usuario
+ */
+public class FormClientesController implements Initializable {
+    private Main stage;
+    private int op;
+    
+    private static Connection conexion;
+    private static PreparedStatement statement;
+    
+    @FXML
+    Button btnGuardar, btnCancelar;
+    
+    @FXML
+    TextField tfClienteId, tfNombre, tfApellido, tfTelefono, tfDireccion, tfNit;
+    
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        if(ClienteDTO.getClienteDTO().getCliente() != null){
+            cargarDatos(ClienteDTO.getClienteDTO().getCliente());
+        }
+    }
+
+    public Main getStage() {
+        return stage;
+    }
+
+    public void setStage(Main stage) {
+        this.stage = stage;
+    }
+    
+    
+    @FXML
+    public void handleButtonAction(ActionEvent event){
+        if(event.getSource() == btnCancelar){
+            stage.menuClientesView();
+        }else if(event.getSource() == btnGuardar){
+            if(op == 1){
+                agregarCliente();
+                stage.menuClientesView();
+            }else if(op == 2){
+                editarCliente();
+                ClienteDTO.getClienteDTO().setCliente(null);
+                stage.menuClientesView();
+            }
+        }
+    }
+    
+    public void cargarDatos(Cliente cliente){
+        tfClienteId.setText(Integer.toString(cliente.getClienteId()));
+        tfNombre.setText(cliente.getNombre());
+        tfApellido.setText(cliente.getApellido());
+        tfTelefono.setText(cliente.getTelefono());
+        tfDireccion.setText(cliente.getDireccion());
+        tfNit.setText(cliente.getNit());        
+    }
+    
+    public void agregarCliente(){
+        try{
+            conexion = Conexion.getInstance().obtenerConexion();
+            String sql = "call sp_agregarClientes(?,?,?,?,?)";
+            statement = conexion.prepareStatement(sql);
+            statement.setString(1, tfNombre.getText());
+            statement.setString(2, tfApellido.getText());
+            statement.setString(3, tfTelefono.getText());
+            statement.setString(4, tfDireccion.getText());
+            statement.setString(5, tfNit.getText());
+            statement.execute();
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }finally{
+            try{
+                if(statement != null){
+                    statement.close();
+                }
+                if(statement != null){
+                    conexion.close();
+                }
+            }catch(SQLException e){
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+    
+    public void editarCliente(){
+        try{
+            conexion = Conexion.getInstance().obtenerConexion();
+            String sql = "call sp_editarClientes(?,?,?,?,?,?)";
+            statement = conexion.prepareStatement(sql);
+            statement.setInt(1, Integer.parseInt(tfClienteId.getText()));
+            statement.setString(2, tfNombre.getText());
+            statement.setString(3, tfApellido.getText());
+            statement.setString(4, tfTelefono.getText());
+            statement.setString(5, tfDireccion.getText());
+            statement.setString(6, tfNit.getText());
+            statement.execute();
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }finally{
+            try{
+                if(statement != null){
+                    statement.close();
+                }
+                if(conexion != null){
+                    conexion.close();
+                }
+            }catch(SQLException e){
+                System.out.println(e.getMessage());
+            }finally{
+            
+            }
+        }
+    }
+    
+    public void setOp(int op) {
+        this.op = op;
+    }   
+    
+}
---
+++ src/org/jorgeperalta/system/Main.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.jorgeperalta.system;
+
+import java.io.InputStream;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.fxml.JavaFXBuilderFactory;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import org.jorgeperalta.controller.FormClientesController;
+import org.jorgeperalta.controller.MenuClientesController;
+import org.jorgeperalta.controller.MenuPrincipalController;
+import org.jorgeperalta.controller.MenuTicketSoporteController;
+
+
+/**
+ *
+ * @author Usuario
+ */
+public class Main extends Application {
+    private Stage stage;
+    private Scene scene;
+    private final String URLVIEW = "/org/jorgeperalta/view/";
+    @Override
+    public void start(Stage stage) throws Exception {
+        this.stage = stage;
+        stage.setTitle("Super Kinal APP");
+        menuPrincipalView();
+        stage.show();
+    }
+    
+    public Initializable switchScene(String fxmlName, int width, int height)throws Exception{
+        Initializable resultado = null;
+        FXMLLoader loader = new FXMLLoader();
+        
+        InputStream file = Main.class.getResourceAsStream("/org/jorgeperalta/view/" + fxmlName);
+        loader.setBuilderFactory(new JavaFXBuilderFactory());
+        loader.setLocation(Main.class.getResource(URLVIEW + fxmlName));
+        
+        scene = new Scene((AnchorPane)loader.load(file), width, height);
+        stage.setScene(scene);
+        stage.sizeToScene();
+        
+        resultado = (Initializable)loader.getController();
+        return resultado;
+    }
+    public void menuPrincipalView(){
+        try{
+            MenuPrincipalController menuPrincipalView = (MenuPrincipalController)switchScene("MenuPrincipalView.fxml",900, 700);
+            menuPrincipalView.setStage(this);
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public void menuClientesView(){
+        try {
+            MenuClientesController menuClientesView = (MenuClientesController)switchScene("MenuClientesView.fxml", 1200, 750);
+            menuClientesView.setStage(this);
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public void formClientesView(int op){
+        try{
+            FormClientesController formClientesView = (FormClientesController)switchScene("FormClientesView.fxml", 500, 600);
+            formClientesView.setOp(op);
+            formClientesView.setStage(this);
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public void menuTicketSoporteView(){
+        try{
+            MenuTicketSoporteController menuTicketSoporteView = (MenuTicketSoporteController)switchScene("MenuTicketSoporteView.fxml", 1200, 750);
+            menuTicketSoporteView.setStage(this);
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
