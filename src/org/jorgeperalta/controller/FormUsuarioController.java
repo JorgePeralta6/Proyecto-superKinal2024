b1f8de8bb126f76d76ffe8f3ca4af5269bd185c4
JorgePeralta6 <jperalta-2023478@kinal.edu.gt>
2024-05-17 08:35:09 -0600

    Nuevo login

--- src/org/jorgeperalta/controller/FormUsuarioController.java
+++ src/org/jorgeperalta/controller/FormUsuarioController.java
@@ -12,12 +12,14 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import org.jorgeperalta.dao.Conexion;
 import org.jorgeperalta.model.NivelAcceso;
+import org.jorgeperalta.model.Empleado;
 import org.jorgeperalta.system.Main;
 import org.jorgeperalta.utils.PasswordUtils;
 
@@ -44,7 +46,13 @@ public class FormUsuarioController implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
         
     }    
-
+    
+    @FXML
+    public void handleButtonAction(ActionEvent event){
+        if(event.getSource() == btnRegistrar){
+            agregarUsuario();
+        }
+    }
 
     public void setStage(Main stage) {
         this.stage = stage;
@@ -57,9 +65,21 @@ public class FormUsuarioController implements Initializable {
             statement = conexion.prepareStatement(sql);
             statement.setString(1, tfUsuario.getText());
             statement.setString(2, PasswordUtils.getInstance().encryptedPassword(tfPassword.getText()));
-            statement.setInt(3, ((NivelAcceso)cmbNivelAcceso.getSelectionModel().getSelectedItem()));
+            statement.setInt(3, ((NivelAcceso)cmbNivelAcceso.getSelectionModel().getSelectedItem()).getNivelAccesoId());
+            statement.setInt(4, ((Empleado)cmbEmpleado.getSelectionModel().getSelectedItem()).getEmpleadoId());
         }catch(SQLException e){
             System.out.println(e.getMessage());
+        }finally{
+            try{
+                if(statement != null){
+                    statement.close();
+                }
+                if(conexion != null){
+                    conexion.close();
+                }
+            }catch(SQLException e){
+                System.out.println(e.getMessage());
+            }
         }
     }
 }
